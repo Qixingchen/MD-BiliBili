2e9c5a61e50c1aeb483fbdc6b45bba1dd13090de
Harlber <1044698769@qq.com>
2015-06-21 17:49:20 +0800

    add FAB animation

--- MDBilibili/app/src/main/java/me/qixingchen/mdbilibili/MainActivity.java
+++ MDBilibili/app/src/main/java/me/qixingchen/mdbilibili/MainActivity.java
@@ -8,68 +8,107 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import me.qixingchen.mdbilibili.model.Recommend;
+import me.qixingchen.mdbilibili.network.GetRecommend;
 
-public class MainActivity extends AppCompatActivity {
-	private static final String TAG = "MainActivity";
-	private DrawerLayout mDrawerLayout;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-		setSupportActionBar(toolbar);
+public class MainActivity extends AppCompatActivity implements GetRecommend.RecommendCallBack {
+    private static final String TAG = "MainActivity";
+    private DrawerLayout mDrawerLayout;
+    private RecyclerView mRecyclerView;
 
-		final ActionBar ab = getSupportActionBar();
-		ab.setHomeAsUpIndicator(R.mipmap.ic_menu);
-		ab.setDisplayHomeAsUpEnabled(true);
+    private Recommend recommend = null;
+    private CardAdapter mCardAdapter;
 
-		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-		NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-		if (navigationView != null) {
-			setupDrawerContent(navigationView);
-		}
-		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-		fab.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
-						.setAction("Action", null).show();
-			}
-		});
-	}
+        final ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.mipmap.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
-	}
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mRecyclerView = (RecyclerView) findViewById(R.id.dast_recycler_view);
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-			case android.R.id.home:
-				mDrawerLayout.openDrawer(GravityCompat.START);
-				return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        mCardAdapter = new CardAdapter(recommend, this.getApplication());
+        mRecyclerView.setAdapter(mCardAdapter);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        if (navigationView != null) {
+            setupDrawerContent(navigationView);
+        }
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "正在刷新", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        setRequest();
+        toolbar.post(new Runnable() {
+            @Override
+            public void run() {
+                ScrollManager manager = new ScrollManager();
+                manager.attach(mRecyclerView);
+                manager.addView(fab, ScrollManager.Direction.DOWN);
+                manager.setInitialOffset(toolbar.getHeight());
+            }
+        });
+        initDrawer();
+    }
 
-	private void setupDrawerContent(NavigationView navigationView) {
-		navigationView.setNavigationItemSelectedListener(
-				new NavigationView.OnNavigationItemSelectedListener() {
-					@Override
-					public boolean onNavigationItemSelected(MenuItem menuItem) {
-						menuItem.setChecked(true);
-						mDrawerLayout.closeDrawers();
-						return true;
-					}
-				});
-	}
+    void setRequest() {
+        GetRecommend.getRecommend().setCallBack(this).GetRecommendInfo("1");
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+    }
+
+    void initDrawer() {
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void recommendCallBack(Recommend recommend) {
+        this.recommend = recommend;
+        mCardAdapter.notifyDateChanged(recommend);
+    }
 }
