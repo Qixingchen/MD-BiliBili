1524baeebb3331c110ac20d35a777e4aa4341055
Harlber <1044698769@qq.com>
2015-07-30 23:54:44 +0800

    detail page ,something bad

--- MDBilibili/app/src/main/java/me/qixingchen/mdbilibili/CardAdapter.java
+++ MDBilibili/app/src/main/java/me/qixingchen/mdbilibili/CardAdapter.java
@@ -18,51 +18,48 @@ import me.qixingchen.mdbilibili.network.GetVolley;
  */
 public class CardAdapter extends RecyclerView.Adapter<BilibiliCardViewHolder> {
 
-	private Recommend recommend;
-	private ImageLoader mImageLoader;
-	private Context mContext;
+    private Recommend recommend;
+    private ImageLoader mImageLoader;
+    private Context mContext;
 
-	public CardAdapter(Recommend recommend, Context context) {
-		this.recommend = recommend;
-		mContext = context;
-		mImageLoader = GetVolley.getmInstance(mContext).getImageLoader();
-	}
+    public CardAdapter(Recommend recommend, Context context) {
+        this.recommend = recommend;
+        mContext = context;
+        mImageLoader = GetVolley.getmInstance(mContext).getImageLoader();
+    }
 
-	@Override
-	public BilibiliCardViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-		View v = LayoutInflater.from(parent.getContext())
-				.inflate(R.layout.dast_bilibili_card_item, parent, false);
-		BilibiliCardViewHolder vh = new BilibiliCardViewHolder(v);
-		return vh;
-	}
+    @Override
+    public BilibiliCardViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.dast_bilibili_card_item, parent, false);
+        BilibiliCardViewHolder vh = new BilibiliCardViewHolder(v);
+        return vh;
+    }
 
-	@Override
-	public void onBindViewHolder(BilibiliCardViewHolder holder, final int position) {
-		holder.textView.setText(recommend.getLists().get(position).getTitle());
-		holder.imageView.setImageUrl(recommend.getLists().get(position).getPic(), mImageLoader);
-		holder.rootView.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-                Intent intent = new Intent(BilibiliApplication.getApplication(),BilibiliDetail.class);
-                String url = String.valueOf(recommend.getLists().get(position).getPic());
-                String title = String.valueOf(recommend.getLists().get(position).getTitle());
-                String aid = String.valueOf(recommend.getLists().get(position).getAid());
-                intent.putExtra("IMG_URL", url);
-                intent.putExtra("TITLE", title);
-                intent.putExtra("AID", aid);
-                mContext.startActivity(intent);
-			}
-		});
-	}
+    @Override
+    public void onBindViewHolder(BilibiliCardViewHolder holder, final int position) {
+        holder.textView.setText(recommend.getLists().get(position).getTitle());
+        holder.imageView.setImageUrl(recommend.getLists().get(position).getPic(), mImageLoader);
+        holder.rootView.setOnClickListener(v -> {
+            Intent intent = new Intent(BilibiliApplication.getApplication(), BilibiliDetail.class);
+            String url = String.valueOf(recommend.getLists().get(position).getPic());
+            String title = String.valueOf(recommend.getLists().get(position).getTitle());
+            String aid = String.valueOf(recommend.getLists().get(position).getAid());
+            intent.putExtra("IMG_URL", url);
+            intent.putExtra("TITLE", title);
+            intent.putExtra("AID", aid);
+            mContext.startActivity(intent);
+        });
+    }
 
-	@Override
-	public int getItemCount() {
-		return recommend == null ? 0 : recommend.getLists().size();
-	}
+    @Override
+    public int getItemCount() {
+        return recommend == null ? 0 : recommend.getLists().size();
+    }
 
-	//刷新数据
-	public void notifyDateChanged(Recommend recommend) {
-		this.recommend = recommend;
-		this.notifyDataSetChanged();
-	}
+    //刷新数据
+    public void notifyDateChanged(Recommend recommend) {
+        this.recommend = recommend;
+        this.notifyDataSetChanged();
+    }
 }
